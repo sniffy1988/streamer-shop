2bfdac4ab02a87154feb3fd465871e59e313f7ee
Anton <sniffy1988@gmail.com>
2018-11-20 23:16:21 +0200

    remove unneed vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="app">
     <div class="ui two item menu">
-      <router-link to="/" class="item" active-class="active" exact="true">Home</router-link>
+      <router-link to="/" class="item" active-class="active" exact>Home</router-link>
       <router-link to="/shop" class="item" active-class="active">Shop</router-link>
-    </div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/shop">Shop</router-link>
     </div>
     <router-view/>
   </div>
